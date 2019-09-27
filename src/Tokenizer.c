f4d9915eed74ad3970679793ee8c04ad9ade9326
Alex <alextrainham@gmail.com>
2019-09-26 20:53:44 -0400

    not really a commit

--- src/Tokenizer.c
+++ src/Tokenizer.c
@@ -5,6 +5,7 @@ _Bool tokenizer(struct lexics *aLex, int *numLex, FILE *inf){
     char str[MY_CHAR_MAX]; 
     
     while(fgets(str, MY_CHAR_MAX, inf) != NULL){ 
+        // create a lexics struct 
         strcpy(aLex[*numLex].lexeme, str);  
         ++ *numLex; 
     }
