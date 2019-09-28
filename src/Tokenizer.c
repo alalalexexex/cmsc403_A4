d4c36b17fbfa4c376421fa505e51be0eb68a5ec8
Alex <alextrainham@gmail.com>
2019-09-28 00:48:55 -0400

    serious changes lmao

--- src/Tokenizer.c
+++ src/Tokenizer.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include "Tokenizer.h"
 
+#define isDigit(c) ('0' <= c && c <= '9')
+#define isLetter(c) ('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z' || c == '_')
+
 void addToLexics(struct lexics * aLex, int * numLex, char * lexeme){
     struct lexics current; 
     strcpy(current.lexeme, lexeme); 
@@ -10,89 +13,61 @@ void addToLexics(struct lexics * aLex, int * numLex, char * lexeme){
     (*numLex)++; 
 }
 
-void parseBracket(struct lexics * aLex, char * p, int * numLex, char * bracket){
-    int length; 
-
-    //printf("the new string p is %s \n", p); 
-
-    if((length = strcspn(p, bracket)) == 0){
-        // only add paren
-        printf("in first if with %s \n", p);
-        addToLexics(aLex, numLex, bracket); 
-    }else{
-        // add before paren 
-        char newLexeme[length]; 
-        strncpy(newLexeme, p, length); 
-        addToLexics(aLex, numLex, newLexeme);
-        // add the actual paren
-        addToLexics(aLex, numLex, bracket);
+char * getNumber(char * currLine, int * position){
+    char * num = malloc(MY_CHAR_MAX * sizeof(char)); 
+    num[MY_CHAR_MAX - 1] = '\0'; 
+    int index = 0; 
+    while(isDigit(currLine[*position])){
+        num[index] = currLine[*position]; 
+        index++; 
+        (*position)++; 
     }
-    
-    //printf("the difference is %ld \n", strlen(result) - length); 
-    // i think I'm doing incorrect math
-    strncpy(p, p + length +1, strlen(p) - length);
-    //p = strtok(NULL, bracket); 
-    // check if the next thing is blank
-    if(p[0] == '\0' || p[0] == '\n' || p[0] == '\t'){
-        p = strtok(NULL, " \t\n"); 
-    }
-
-    //printf("result is: %s\n",p); 
+    (*position)--; 
+    return num;  
 }
+
+char * getIdentifier(char * currLine, int * position){
+    char * ident = malloc(MY_CHAR_MAX * sizeof(char)); 
+    ident[MY_CHAR_MAX - 1] = '\0'; 
+    int index = 0; 
+    while(isLetter(currLine[*position])){
+        ident[index] = currLine[*position]; 
+        index++; 
+        (*position)++; 
+    }
+    (*position)--; 
+    return ident; 
+}
+
+char * makeSingleLex(char lex){
+    char * single = malloc(2 * sizeof(char)); 
+    single[0] = lex; 
+    single[1] = '\0'; 
+    return single; 
+}
+
 void lineParse(struct lexics * aLex, char * currLine, int *numLex){
     int start = 0; 
-    //printf("line length is: %ld\n", strlen(currLine));
     int charCounter = 0; 
     for(int i =0; i < strlen(currLine); ++i){
-        if(!isalpha(currLine[i])){
-            if(charCounter == 0){
-                continue; 
-            }else{
-                char * currentIdent = malloc(MY_CHAR_MAX * sizeof(char)); 
-                currentIdent[MY_CHAR_MAX - 1] = '\0'; 
-                strncpy(currentIdent, (currLine + i) - charCounter, charCounter); 
-                printf("%s\n",  currentIdent); 
-                free(currentIdent); 
-            }
-            charCounter = 0; 
-            // char str[i - start +1]; 
-            // str[i -start +1] = '\0';
-            // strncpy(str, currLine + start, i - start + 1); 
-            // printf("%s\n", str); 
-            // start = i + 1; 
-        }else if(isalpha(currLine[i])){
-            charCounter ++; 
-            //printf("%c", currLine[i]); 
+        if(isLetter(currLine[i])){
+            addToLexics(aLex, numLex, getIdentifier(currLine, &i));   
+        }else if(isDigit(currLine[i])){
+            addToLexics(aLex, numLex, getNumber(currLine, &i)); 
+        }else if(currLine[i] == '{'){
+            addToLexics(aLex, numLex, makeSingleLex(currLine[i])); 
+        }else if(currLine[i] == ';'){
+            addToLexics(aLex, numLex, makeSingleLex(currLine[i])); 
+        }else if(currLine[i] == ','){
+            addToLexics(aLex, numLex, makeSingleLex(currLine[i])); 
+        }else if(currLine[i] == '}'){
+            addToLexics(aLex, numLex, makeSingleLex(currLine[i])); 
+        }else if(currLine[i] == '('){
+            addToLexics(aLex, numLex, makeSingleLex(currLine[i])); 
+        }else if(currLine[i] == ')'){
+            addToLexics(aLex, numLex, makeSingleLex(currLine[i])); 
         }
     }
-    // char *p = strtok(currLine, " \t\n");
-    // while(p != NULL){
-    //     struct lexics current; 
-    //     int length; 
-    //     // check if p has  a bracket 
-    //     if(strstr(p, "(") != NULL){
-    //         parseBracket(aLex, p, numLex, "(");   
-    //     }else if(strstr(p, ")") != NULL){
-    //         parseBracket(aLex, p, numLex, ")");  
-    //     }else if(strstr(p, "}") != NULL){
-    //         parseBracket(aLex, p, numLex, "}");  
-    //     }else if(strstr(p, "{") != NULL){
-    //         parseBracket(aLex, p, numLex, "{");  
-    //     }else if(strstr(p, "=") != NULL){
-    //         parseBracket(aLex, p, numLex, "=");
-    //     }else if(strstr(p, "+") != NULL){
-    //         parseBracket(aLex, p, numLex, "+");
-    //     }else if(strstr(p, ",") != NULL){
-    //         parseBracket(aLex, p, numLex, ","); 
-    //     }else if(strstr(p, ";") != NULL){
-    //         parseBracket(aLex, p, numLex, ";");  
-    //     }
-    //     else{
-    //         addToLexics(aLex, numLex, p); 
-    //         p = strtok(NULL, " \t\n");
-    //     }  
-    //     printf("p is %s\n", p); 
-    // }
 }
 
 _Bool tokenizer(struct lexics *aLex, int *numLex, FILE *inf){
